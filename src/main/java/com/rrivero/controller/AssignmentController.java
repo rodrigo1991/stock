184889ed1479a86145d4c9601e3d2dfd0713fd94
rodrigo <ro.rivero1991@gmail.com>
2019-02-06 02:25:48 -0300

    cambios

--- src/main/java/com/rrivero/controller/AssignmentController.java
+++ src/main/java/com/rrivero/controller/AssignmentController.java
@@ -3,14 +3,18 @@ package com.rrivero.controller;
 import com.rrivero.exception.ResourceNotFoundException;
 import com.rrivero.model.Assignment;
 import com.rrivero.repository.AssignmentRepository;
+import com.rrivero.repository.BranchRepository;
+import com.rrivero.repository.UserRepository;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -24,7 +28,13 @@ import javax.validation.Valid;
 public class AssignmentController {
 
 	@Autowired
+	private BranchRepository branchRepository;
+	
+	@Autowired
 	private AssignmentRepository assignmentRepository;
+	
+	@Autowired
+	private UserRepository userRepository;
 
 	
 	@GetMapping()
@@ -39,14 +49,30 @@ public class AssignmentController {
 	}
 	
 	@PutMapping("{assignmentId}")
-	public ResponseEntity<?> updateAssignment(@PathVariable Long assignmentId,  @Valid @RequestBody Assignment assignmentRequest) {
+	public ResponseEntity<Assignment> updateAssignment(@PathVariable Long assignmentId,  @Valid @RequestBody Assignment assignmentRequest) {
+		
+		if (!this.branchRepository.existsById(assignmentRequest.getBranch().getId())) {
+			throw new ResourceNotFoundException("Branch " + assignmentRequest.getBranch().getId() + " not found");
+		}
+		
+		if (!this.userRepository.existsById(assignmentRequest.getUser().getId())) {
+			throw new ResourceNotFoundException("User " + assignmentRequest.getUser().getId() + " not found");
+		}
 		
 		return this.assignmentRepository.findById(assignmentId).map(assignment -> {
 			assignment.setBegin(assignmentRequest.getBegin());
 			assignment.setEnd(assignmentRequest.getEnd());
-			this.assignmentRepository.save(assignment);
-			return ResponseEntity.noContent().build();
+			assignment.setUser(assignmentRequest.getUser());
+			assignment.setBranch(assignmentRequest.getBranch());
+			Assignment savedAssignment = this.assignmentRepository.save(assignment);
+			return ResponseEntity.ok(savedAssignment);
 		}).orElseThrow(() -> new ResourceNotFoundException("assignmentId " + assignmentId + " not found"));
+	}
+	
+	@PostMapping()
+	public ResponseEntity<Assignment> create(@RequestBody Assignment assignment) {
+		Assignment savedAssignment= this.assignmentRepository.save(assignment);	
+		return new ResponseEntity<Assignment>(savedAssignment, HttpStatus.CREATED);
 	}
 
 	
